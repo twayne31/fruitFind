e9f4c63a436bb5f13df17d09f0166d72942f475c
Taylor Hampton <taylor@curateurs.com>
2018-04-24 15:31:39 -0400

    css overlay features

--- JS/fruits.js
+++ JS/fruits.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
     $(".orange").on("click", function(){
     alert("this fruit is good during the summer.")
     })
-    
+    $(".pears").on("click", function (){
+    alert("pears on deck")    
+    }) 
  });
 
 
